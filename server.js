2b6b891edbeba102e88716b3754f48d79dd99da1
Chamika Dilshan <chamidilshan55@gmail.com>
2024-01-26 10:00:03 +0530

    started project and created structure

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());    
+
+const listener = app.listen(process.env.PORT || 5000, () => {
+    console.log('App is listening on port ' + listener.address().port)
+})
+
