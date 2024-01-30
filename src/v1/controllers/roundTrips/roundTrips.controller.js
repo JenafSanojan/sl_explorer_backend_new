184a8ef6bfcb5ddae5e8c053317692d8f79855eb
Chamika Dilshan <chamidilshan55@gmail.com>
2024-01-30 22:03:18 +0530

    fixed live issues

--- src/v1/controllers/roundTrips/roundTrips.controller.js
+++ src/v1/controllers/roundTrips/roundTrips.controller.js
@@ -1,46 +1,54 @@
 const RoundTripsModel = require("../../models/roundTrips/roundTrips.mongo");
 
-const createRoundTrip = async (req, res) => {
-  try {
-    if (
-      !req.body.packageName ||
-      !req.body.packageShortDescription ||
-      !req.body.packageCoverDescription ||
-      !req.body.packageCoverImage ||
-      !req.body.packageImageLinks ||
-      !req.body.packageTitle ||
-      !req.body.packageSubTitle ||
-      !req.body.packageTotalSeats ||
-      !req.body.itenary ||
-      !req.body.hotels ||
-      !req.body.prices
-    ) {
-      return res.status(400).send({ message: "Request body is missing!" });
-    }
+const createRoundTrip = async (req, res) =>{
+  try{
 
-    const newRoundTrip = {
-      packageName: req.body.packageName,
-      packageShortDescription: req.body.packageShortDescription,
-      packageCoverDescription: req.body.packageCoverDescription,
-      packageCoverImage: req.body.packageCoverImage,
-      packageImageLinks: req.body.packageImageLinks,
-      packageTitle: req.body.packageTitle,
-      packageSubTitle: req.body.packageSubTitle,
-      packageTotalSeats: req.body.packageTotalSeats,
-      itenary: req.body.itenary,
-      hotels: req.body.hotels,
-      prices: req.body.prices,
-    };
+      if(!req.body.packageName ||
+          !req.body.packageShortDescription ||
+          !req.body.packageCoverDescription || 
+          !req.body.packageCoverImage ||
+          !req.body.packageImageLinks ||
+          !req.body.packageTitle ||
+          !req.body.packageSubTitle ||
+          !req.body.packageTotalSeats ||
+          !req.body.itenary ||
+          !req.body.hotels ||
+          !req.body.prices){
+          return res.status(400).send({message: "Request body is missing!"});
+      } 
+
+      const newRoundTrip = {
+          packageName : req.body.packageName,
+          packageShortDescription : req.body.packageShortDescription,
+          packageCoverDescription : req.body.packageCoverDescription,
+          packageCoverImage : req.body.packageCoverImage,
+          packageImageLinks : req.body.packageImageLinks,
+          packageTitle : req.body.packageTitle, 
+          packageSubTitle : req.body.packageSubTitle,
+          packageTotalSeats : req.body.packageTotalSeats,
+          itenary : req.body.itenary,
+          hotels : req.body.hotels, 
+          prices : req.body.prices
+      }
+
+      const roundTrip = await RoundTripsModel.create(newRoundTrip);
+      return res.status(201).send({status: 'success', data: roundTrip});
+
+  } catch(err){
+      res.status(500).json({message: err.message});
+  }
+}
 
 
 const getRoundTrips = async (req, res) =>{ 
-    try{
-        const data = await RoundTripsModel.find().populate('hotels.hotel').exec();
-        res.json(data);
-    } catch(err){
-        res.status(500).json({message: err.message});
-    }
+  try{
+      const data = await RoundTripsModel.find().populate('hotels.hotel').exec();
+      res.json(data);
+  } catch(err){
+      res.status(500).json({message: err.message});
+  }
 }
+
 
 const getRoundTripPackage = async (req, res) =>{
     try{ 
@@ -52,7 +60,6 @@ const getRoundTripPackage = async (req, res) =>{
 }
 
 module.exports = {
-
     createRoundTrip,
     getRoundTrips,
     getRoundTripPackage
