9b52332bdefa255936c528fb469c4e60ec49f2a4
JenafSanojan <93027811+JenafSanojan@users.noreply.github.com>
2024-01-30 21:13:31 +0530

    findAnOrder_Ok

--- src/v1/routes/orders/orders.router.js
+++ src/v1/routes/orders/orders.router.js
@@ -10,16 +10,3 @@ router.get("/", ordersController.getOrders);
 router.get("/:customerId", ordersController.getOrderByCustomerId);
 
 module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// const ordersController = require("../../controllers/orders/orders.controller");
-
-// // POST method
-// router.post("/", ordersController.createOrder);
-// //GET method
-// router.get("/", ordersController.getOrders);
-
-// router.get("/:customerId", ordersController.getOrderByCustomerId);
-
-// module.exports = router;
