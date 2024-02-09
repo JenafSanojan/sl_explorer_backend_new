77b430f88c04b32b8bef7a07fc8a4299a4326525
Chamika Dilshan <chamidilshan55@gmail.com>
2024-02-09 07:29:08 +0530

    added creating admin api

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ require("dotenv").config();
 const hotelsRouter = require("./src/v1/routes/hotels/hotels.router");
 const roundTripsRouter = require("./src/v1/routes/roundTrips/roundTrips.router");
 const ordersRouter = require("./src/v1/routes/orders/orders.router");
+const adminsRouter = require("./src/v1/routes/admins/admins.router");
 const cors = require("cors");
+const admin = require("firebase-admin");
+const serviceAccount = require("./sl-explorer-firebase-adminsdk-f1771-328b42f1b0.json");
 
 app.use(express.json());
 
 app.use(cors());
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://console.firebase.google.com/project/sl-explorer/database/sl-explorer-default-rtdb/data/~2F",
+});
+
 app.use("/api/v1/hotels", hotelsRouter);
 app.use("/api/v1/roundTrips", roundTripsRouter);
 app.use("/api/v1/orders", ordersRouter);
+app.use("/api/v1/setAdmin", adminsRouter);
 
 const listener = app.listen(process.env.PORT || 5000, () => {
   console.log("App is listening on port " + listener.address().port);
