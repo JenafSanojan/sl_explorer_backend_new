9d5399448f0ec78c7335cea861c4977f64331058
JenafSanojan <93027811+JenafSanojan@users.noreply.github.com>
2024-02-26 23:30:49 +0530

    RoundTripUpdateAdded

--- src/v1/controllers/roundTrips/roundTrips.controller.js
+++ src/v1/controllers/roundTrips/roundTrips.controller.js
@@ -39,6 +39,48 @@ const createRoundTrip = async (req, res) => {
   }
 };
 
+const updateRoundTrip = async (req, res) => {
+  try {
+    if (
+      !req.body.packageName ||
+      !req.body.packageShortDescription ||
+      !req.body.packageCoverDescription ||
+      !req.body.packageCoverImage ||
+      !req.body.packageImageLinks ||
+      !req.body.packageTitle ||
+      !req.body.packageSubTitle ||
+      !req.body.packageTotalSeats ||
+      !req.body.itenary ||
+      !req.body.hotels ||
+      !req.body.prices
+    ) {
+      return res.status(400).send({ message: "Request body is missing!" });
+    }
+
+    const newRoundTrip = {
+      packageName: req.body.packageName,
+      packageShortDescription: req.body.packageShortDescription,
+      packageCoverDescription: req.body.packageCoverDescription,
+      packageCoverImage: req.body.packageCoverImage,
+      packageImageLinks: req.body.packageImageLinks,
+      packageTitle: req.body.packageTitle,
+      packageSubTitle: req.body.packageSubTitle,
+      packageTotalSeats: req.body.packageTotalSeats,
+      itenary: req.body.itenary,
+      hotels: req.body.hotels,
+      prices: req.body.prices,
+    };
+
+    const roundTrip = await RoundTripsModel.findByIdAndUpdate(
+      req.body.id,
+      newRoundTrip
+    );
+    return res.status(201).send({ status: "success", data: roundTrip });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 const getRoundTrips = async (req, res) => {
   try {
     const data = await RoundTripsModel.find().populate("hotels.hotel").exec();
@@ -61,6 +103,7 @@ const getRoundTripPackage = async (req, res) => {
 
 module.exports = {
   createRoundTrip,
+  updateRoundTrip,
   getRoundTrips,
   getRoundTripPackage,
 };
